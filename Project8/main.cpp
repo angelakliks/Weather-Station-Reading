7f70588845ec7d8c5dd981fc9a689876f9d5c1ba
Angela <angela.kliks@gmail.com>
2021-04-20 18:23:56 -0400

    Initial Commit

---
+++ Project8/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project8
+//
+//  Created by Angela Kliks on 4/20/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
